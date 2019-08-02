e1638b69f42cc09ea49ef70765e9ac8dd743d162
Genie Sun <ext.cheng.sun@ef.com>
2019-08-02 18:17:54 +0800

    update git push shell

--- gitPush.sh
+++ gitPush.sh
@@ -4,8 +4,9 @@ echo "starting commit $* to github"
 if [[ "$*" ]]; then
 	#statements
 	git add .
-git commit -m "$*"
-git push
-echo "finish push 🍺🍺🍺🍺🍺🍺 ✅"
+	git commit -m "$*"
+	git push
+	echo "finish push 🍺🍺🍺🍺🍺🍺 ✅"
+else
+	echo "Empty Commit Message, must write ❌❌❌❌"
 fi
-echo "Empty Commit Message, must write ❌❌❌❌"
